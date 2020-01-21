9ee80b8fe8ac162b66d352bf09aaa09e0a9704d9
yanzhihao <yanzhihao@youzan.com>
2020-01-21 13:20:27 +0800

    feat: add route

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
-import TopBar from './TopBar';
+import { Switch, Route } from 'react-router-dom';
 import { CssBaseline, makeStyles, Theme } from '@material-ui/core';
 import { IMenuItem } from '../../types/menu';
+import TopBar from './TopBar';
 import SideMenu from './SideMenu';
 import useBoolean from '../../hooks/useBoolean';
+import Loadable from '../../components/Loadable';
+
+const getPath = (menuItem: IMenuItem) => {
+  if (Array.isArray(menuItem.path)) {
+    return menuItem.path.length ? menuItem.path[0] : '';
+  }
+  return menuItem.path;
+};
+
+const renderRoute = (
+  menu: IMenuItem,
+  parentPath: string = '',
+): React.ReactElement => {
+  const currentPath = getPath(menu);
+  const completedPath = [
+    '',
+    parentPath.replace(/^\/*/, '').replace(/\/*$/, ''),
+    currentPath.replace(/^\/*/, ''),
+  ].join('/');
+  if ('children' in menu) {
+    return (
+      <React.Fragment key={completedPath}>
+        {menu.children.map(item => renderRoute(item, completedPath))}
+      </React.Fragment>
+    );
+  }
+  return (
+    <Route key={completedPath} path={completedPath}>
+      <Loadable lazyComponent={menu.component} />
+    </Route>
+  );
+};
 
 const useStyles = makeStyles((theme: Theme) => ({
   mainContent: {
@@ -20,7 +53,7 @@ const BasicLayout: React.FC<IBasicLayoutProps> = props => {
   const {
     value: isSideMenuExpended,
     toggle: onSideMenuExpendToggle,
-  } = useBoolean(false);
+  } = useBoolean(true);
   const classes = useStyles();
   return (
     <>
@@ -28,7 +61,7 @@ const BasicLayout: React.FC<IBasicLayoutProps> = props => {
       <TopBar onMenuToggle={onSideMenuExpendToggle} />
       <main className={classes.mainContent}>
         <SideMenu isExpended={isSideMenuExpended} menu={props.menu} />
-        Hello
+        <Switch>{props.menu.map(item => renderRoute(item))}</Switch>
       </main>
     </>
   );
